b15dc57987fee430bda49acf654fb6f3d4f46be9
DFaltGP <skyzzlockzz@gmail.com>
2026-03-29 19:03:27 -0300

    fix(app): change status code from validation behavior from 400 to 422

--- InovaBank.Application/Behaviors/ValidationBehavior.cs
+++ InovaBank.Application/Behaviors/ValidationBehavior.cs
@@ -50,6 +50,6 @@ public sealed class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidat
             .MakeGenericType(resultType)
             .GetMethod("Failure", [typeof(string), typeof(int)]);
 
-        return (TResponse)failureMethod!.Invoke(null, [message, 400])!;
+        return (TResponse)failureMethod!.Invoke(null, [message, 422])!;
     }
 }
