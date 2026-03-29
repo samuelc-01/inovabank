d393770215c1a440a929b0fc09f85c254f7c1678
DFaltGP <skyzzlockzz@gmail.com>
2026-03-29 17:25:33 -0300

    feat(domain): add distributed cache interface

---
+++ InovaBank.Domain/Interfaces/ICacheService.cs
@@ -0,0 +1,8 @@
+namespace InovaBank.Domain.Interfaces;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string key, CancellationToken ct = default);
+    Task SetAsync<T>(string key, T value, TimeSpan? expiration = null, CancellationToken ct = default);
+    Task RemoveAsync(string key, CancellationToken ct = default);
+}
