c841a8c88e4239e1e84f6a49bce7728876ae7f81
DFaltGP <skyzzlockzz@gmail.com>
2026-03-29 19:02:25 -0300

    feat(api): implement get by id and cnpj endpoints

--- InovaBank.Api/Controllers/AccountController.cs
+++ InovaBank.Api/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 using InovaBank.Api.DTOs.Requests;
 using InovaBank.Application.Features.Accounts.Commands.OpenAccount;
+using InovaBank.Application.Features.Accounts.Queries.GetAccountByCnpj;
+using InovaBank.Application.Features.Accounts.Queries.GetAccountById;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,20 @@ public sealed class AccountController(IMediator _mediator) : ApiControllerBase
         var command = new OpenAccountCommand(request.Cnpj, request.Agencia, request.ImagemDocumento);
         var result = await _mediator.Send(command);
 
+        return HandleResult(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] string id)
+    {
+        var result = await _mediator.Send(new GetAccountByIdQuery(id));
+        return HandleResult(result);
+    }
+
+    [HttpGet("cnpj/{cnpj}")]
+    public async Task<IActionResult> GetByCnpj(string cnpj)
+    {
+        var result = await _mediator.Send(new GetAccountByCnpjQuery(cnpj));
         return HandleResult(result);
     }
 }
