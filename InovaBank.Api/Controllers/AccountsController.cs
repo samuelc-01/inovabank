bbecff02d9cc94e12c158e20f7b29e3ea4739b9d
DFaltGP <skyzzlockzz@gmail.com>
2026-03-29 21:22:42 -0300

    feat(api): expose patch and delete endpoints for account management

--- InovaBank.Api/Controllers/AccountsController.cs
+++ InovaBank.Api/Controllers/AccountsController.cs
@@ -1,13 +1,16 @@
 using InovaBank.Api.DTOs.Requests;
+using InovaBank.Application.Features.Accounts.Commands.ChangeStatus;
+using InovaBank.Application.Features.Accounts.Commands.CloseAccount;
 using InovaBank.Application.Features.Accounts.Commands.OpenAccount;
 using InovaBank.Application.Features.Accounts.Queries.GetAccountByCnpj;
 using InovaBank.Application.Features.Accounts.Queries.GetAccountById;
+using InovaBank.Domain.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InovaBank.Api.Controllers;
 
-public sealed class AccountController(IMediator _mediator) : ApiControllerBase
+public sealed class AccountsController(IMediator _mediator) : ApiControllerBase
 {
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] OpenAccountRequest request)
@@ -29,6 +32,20 @@ public sealed class AccountController(IMediator _mediator) : ApiControllerBase
     public async Task<IActionResult> GetByCnpj(string cnpj)
     {
         var result = await _mediator.Send(new GetAccountByCnpjQuery(cnpj));
+        return HandleResult(result);
+    }
+
+    [HttpPatch("{id}/status")]
+    public async Task<IActionResult> ChangeStatus(string id, [FromBody] ChangeStatusRequest request)
+    {
+        var result = await _mediator.Send(new ChangeStatusCommand(id, request.Status));
+        return HandleResult(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Close(string id)
+    {
+        var result = await _mediator.Send(new CloseAccountCommand(id));
         return HandleResult(result);
     }
 }
