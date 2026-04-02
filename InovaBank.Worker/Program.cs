c664b9a5d0fb30d64c29241e2a9055e6264d7850
DFaltGP <skyzzlockzz@gmail.com>
2026-04-02 20:32:13 -0300

    feat(worker): config masstransit and mongodb

--- InovaBank.Worker/Program.cs
+++ InovaBank.Worker/Program.cs
@@ -1,7 +1,24 @@
 using InovaBank.Worker;
+using InovaBank.Worker.Consumers;
+using InovaBank.Worker.Infrastructure;
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddSingleton<MongoContext>();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<TransactionCreatedConsumer>();
+    x.AddConsumer<TransferCreatedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMq"));
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var host = builder.Build();
 host.Run();
